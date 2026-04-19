99cd2bd7a2aec7327e512e5f5bfcd33bcdf5130a
343306 <01206747@pw.edu.pl>
2026-04-19 22:39:40 +0200

    Modify triangulation function to include output parameter

--- src/triangulation.h
+++ src/triangulation.h
@@ -6,6 +6,6 @@ double odl (double x1, double y1, double x2, double y2);
 void check(graf* g, int a, int b, int c, int ac, int bc);
 int find_pkt_pos(graf* g, double x, double y, int n);
 int find_link(graf* g, int a, int b);
-void triangulation(graf* g);
+void triangulation(graf* g, char* out);
 void switch_pkt(graf* g, int m);
 #endif
