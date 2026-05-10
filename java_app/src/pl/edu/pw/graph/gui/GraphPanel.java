c4a0b45cb102d0e263b9117388f1a18dfd7d9cc8
liwiawojtachnik <01206838@pw.edu.pl>
2026-05-10 16:16:11 +0200

    fix: obsługa awarii silnika w Main i dodanie walidacji współrzędnych

--- java_app/src/pl/edu/pw/graph/gui/GraphPanel.java
+++ java_app/src/pl/edu/pw/graph/gui/GraphPanel.java
@@ -1,32 +1,59 @@
 package pl.edu.pw.graph.gui;
 
+import pl.edu.pw.graph.model.Graph;
+import pl.edu.pw.graph.model.Vertex;
+
+
 import javax.swing.*;
 import java.awt.*;
 
 public class GraphPanel extends JPanel {
 
+    private Graph graph;
+
     public GraphPanel() {
         setBackground(Color.WHITE);
+    }
+
+    public void setGraph(Graph graph) {
+        this.graph = graph;
+        this.repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
+        if (graph == null || graph.getVertices() == null) {
+            return;
+        }
+
         Graphics2D g2d = (Graphics2D) g;
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2d.setColor(Color.BLACK);
-        g2d.setStroke(new BasicStroke(2)); // Grubość linii
-        g2d.drawLine(400, 100, 200, 400); // Lewa linia
-        g2d.drawLine(400, 100, 600, 400); // Prawa linia
-        g2d.drawLine(200, 400, 600, 400); // Dolna linia
+        // miejsce na rysowanie krawędzi (w przyszłości)
 
-        
         g2d.setColor(Color.RED);
-        int radius = 15; // Promień wierzchołka
-        g2d.fillOval(400 - radius, 100 - radius, radius * 2, radius * 2); // Górny
-        g2d.fillOval(200 - radius, 400 - radius, radius * 2, radius * 2); // Lewy dolny
-        g2d.fillOval(600 - radius, 400 - radius, radius * 2, radius * 2); // Prawy dolny
+        int radius = 15;
+
+        for (Vertex v : graph.getVertices()) {
+            double x = v.getX();
+            double y = v.getY();
+
+            // zabezpieczenie przed błędami z c
+            if (Double.isNaN(x) || Double.isNaN(y) || Double.isInfinite(x) || Double.isInfinite(y)) {
+                System.err.println("Pominięto uszkodzony wierzchołek z C!");
+                continue;
+            }
+
+            int drawX = (int) x - radius;
+            int drawY = (int) y - radius;
+
+            g2d.fillOval(drawX, drawY, radius * 2, radius * 2);
+
+            g2d.setColor(Color.BLACK);
+            g2d.drawString(String.valueOf(v.getId()), drawX + 35, drawY + 15);
+            g2d.setColor(Color.RED);
+        }
     }
 }
